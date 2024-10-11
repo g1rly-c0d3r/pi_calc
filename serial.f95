971151bcef678bf2f74b27e54eef1c99067bb4c3
g1rly-c0d3r <girly_c0d3r@aol.com>
2024-10-11 09:17:51 -0500

    it compiles now

--- serial.f95
+++ serial.f95
@@ -2,23 +2,37 @@ program calculate_pi
   use FMZM
   implicit none
 
+  type(fm)           :: pi
+  type(im)           :: n
+  character(len=500) :: argv
+
+  call get_command_argument(1, argv)
+  call fm_set(100)
+
+  n = to_im(argv)
+  call im_print(n)
+
+  pi = inverse_pi(n)
+
+  call fm_print(pi)
+
   contains
   
   function inverse_pi(n)
-    integer (kind=selected_int_kind(18)), intent(in) :: n
-    integer (kind=selected_int_kind(18)) :: i
-    type(fm)                             :: inverse_pi
-    type(fm)                             :: y
-    type(im)                             :: mp_i
+    type(im), intent(in) :: n
+    type(fm)             :: inverse_pi
+    type(fm)             :: y
+    type(im)             :: i
 
     y = 0
+    i = 0
 
-    do i = 0, n
-      mp_i = to_im(i)
+    do while(i.lt.n)
+      i = i + 1
 
-      y = ( (-1)**i * gamma(to_fm((6*mp_i) + 1)) )&
-          / (gamma(to_fm((3*mp_i)+1)) * gamma(to_fm(mp_i+1)) **3 )&
-          * ( (13591409 + 545140134*i)/640320**((3*n+3)/2) )
+      y = ( (-1)**i * gamma(to_fm((6*i) + 1)) )&
+          / (gamma(to_fm((3*i)+1)) * gamma(to_fm(i+1)) **3 )&
+          * ( (13591409 + 545140134*i)/640320**((3*i+3)/2) )
     end do
 
     inverse_pi = 12 * y
