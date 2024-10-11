e206ed5ea27089e17e5c27ba1aed9a291d048d06
g1rly-c0d3r <girly_c0d3r@aol.com>
2024-10-11 11:07:35 -0500

    it runs, but no pi

--- serial.f95
+++ serial.f95
@@ -7,32 +7,38 @@ program calculate_pi
   character(len=500) :: argv
 
   call get_command_argument(1, argv)
-  call fm_set(100)
+  call fm_set(10)
 
   n = to_im(argv)
-  call im_print(n)
 
-  pi = inverse_pi(n)
+  pi = 1 / inverse_pi(n)
 
   call fm_print(pi)
+  call fm_print(1/pi)
 
   contains
   
   function inverse_pi(n)
     type(im), intent(in) :: n
     type(fm)             :: inverse_pi
-    type(fm)             :: y
+    type(fm)             :: y, factnum, factdenom, num, denom
     type(im)             :: i
 
     y = 0
     i = 0
 
-    do while(i.lt.n)
-      i = i + 1
+    do while(i.le.n)
 
-      y = ( (-1)**i * gamma(to_fm((6*i) + 1)) )&
-          / (gamma(to_fm((3*i)+1)) * gamma(to_fm(i+1)) **3 )&
-          * ( (13591409 + 545140134*i)/640320**((3*i+3)/2) )
+      factnum = (-1)**i * gamma(to_fm(6*i + 1))
+      factdenom = (gamma(to_fm(3*i + 1)) * (gamma(to_fm(i + 1))**3) )
+      denom = 640320**((3*i+3)/2) 
+      num =13591409 + 545140134*i 
+
+      y = y + ((factnum/factdenom)*(num/denom))
+      
+      call fm_print(y)
+
+      i = i + 1
     end do
 
     inverse_pi = 12 * y
